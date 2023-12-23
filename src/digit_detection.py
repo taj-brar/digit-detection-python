8db51ffa5b84825a8a5b30708413f743e87ef353
Taj Brar <taj.brar3047@gmail.com>
2023-12-22 20:32:42 -0600

    Added model persistence

--- src/digit_detection.py
+++ src/digit_detection.py
@@ -1,16 +1,28 @@
 from sklearn.tree import DecisionTreeClassifier
+import joblib
 import pandas as pd
+import os
 
 
 # CONSTANTS
 TRAIN_FILE = "sample_data/train.csv"
 TEST_FILE = "sample_data/test.csv"
+MODEL_FILE = "sample_data/model.joblib"
 
 
 def main():
-    model = train_model()
-    results = model.predict([get_testing_data()])
+    model = get_model()
+    results = model.predict(get_testing_data())
     print(results)
+
+
+def get_model():
+    if os.path.isfile(MODEL_FILE):
+        print("Found existing model.")
+        return joblib.load(MODEL_FILE)
+    else:
+        print("Building new model...")
+        return train_model()
 
 
 def train_model():
@@ -19,7 +31,10 @@ def train_model():
 
     print("Training model...")
     model.fit(samples, targets)
-    print("Model trained.\n")
+    print("Model trained.")
+
+    joblib.dump(model, MODEL_FILE)
+    print("Model saved.")
 
     return model
 
@@ -39,7 +54,7 @@ def get_testing_data():
     test_df = pd.read_csv(TEST_FILE)
     print("Retrieved testing samples.")
 
-    return test_df.values[0]
+    return test_df.values
 
 
 if __name__ == "__main__":
